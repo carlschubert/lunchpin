44f9a47dc107c296d5b59ec40d3da1f355da77ca
Carl Schubert <carl.schubert@gmail.com>
2017-07-24 22:12:13 -0400

    chore: Require bootstrap

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
 
+import App from './components/App';
 import './static/images/favicon.ico';
+import './style.scss';
 
-console.log('hello world');
+ReactDOM.render(<App />, document.getElementById('root'));
